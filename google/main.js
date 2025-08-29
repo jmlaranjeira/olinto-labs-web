ed5291a58245b018b1a3e415a27267231bab8c79
jmlaranjeira <juanma_lgm@hotmail.com>
2025-08-29 16:09:49 +0200

    Delete google/main.js

--- google/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './assets/styles/tailwind.css'; // Importa Tailwind
-import './assets/styles/base.scss'; // Importa estilos base SCSS
-
-createApp(App).mount('#app');
