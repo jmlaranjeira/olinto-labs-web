aa922701937b66d7e9687ca1070f2e3b68c628c4
jmlaranjeira <juanma_lgm@hotmail.com>
2025-08-29 16:01:29 +0200

    Set up main.js for Vue application
    Initialize Vue app with Tailwind and SCSS styles.

---
+++ google/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/styles/tailwind.css'; // Importa Tailwind
+import './assets/styles/base.scss'; // Importa estilos base SCSS
+
+createApp(App).mount('#app');
